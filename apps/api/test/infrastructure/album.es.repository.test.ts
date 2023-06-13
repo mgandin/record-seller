e8ea699c551cc235511aeca84fdd0bab3e606bec
mathieugandin <mathieu.gandin@appenin.fr>
2023-06-13 15:04:19 +0200

    tech: add more tests for usecase and mock ES for repository test

--- apps/api/test/infrastructure/album.es.repository.test.ts
+++ apps/api/test/infrastructure/album.es.repository.test.ts
@@ -4,6 +4,7 @@ import { Client } from "@elastic/elasticsearch";
 import * as fs from "fs";
 import { AlbumElasticSearchRepository } from "../../src/infrastructure/album.es.repository";
 import { config } from "../../src/config";
+import ClientMock from "@elastic/elasticsearch-mock";
 
 describe("Album Elastic Search Repository - test", () => {
   test("#index and search", async () => {
@@ -19,16 +20,23 @@ describe("Album Elastic Search Repository - test", () => {
       year: 1989,
     };
 
+    const mock = new ClientMock();
+    mock.add(
+      {
+        method: ["GET", "POST"],
+        path: ["/:index/_doc", "/:index/_search", "/:index/_refresh"],
+      },
+      () => {
+        return {
+          hits: {
+            hits: [{ _source: album }],
+          },
+        };
+      }
+    );
     const client = new Client({
       node: config.ES_URL,
-      auth: {
-        username: config.ELASTIC_USER,
-        password: config.ELASTIC_PASSWORD,
-      },
-      tls: {
-        ca: fs.readFileSync(config.ELASTIC_CA_FILE),
-        rejectUnauthorized: false,
-      },
+      Connection: mock.getConnection(),
     });
     const albumElasticSearchRepository = new AlbumElasticSearchRepository(
       client
@@ -42,14 +50,5 @@ describe("Album Elastic Search Repository - test", () => {
       album.name
     );
     expect(albumSearched).toStrictEqual(album);
-
-    await client.deleteByQuery({
-      index: "albums",
-      query: {
-        match: {
-          name: album.name,
-        },
-      },
-    });
   });
 });
